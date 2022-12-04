cd04be79f2459c232b7a79935f092a6f7a443815
Craig Pettifor <craig_petti4@hotmail.com>
2022-12-04 17:54:13 +0100

    tracks minutes

--- timing.py
+++ timing.py
@@ -1,1 +1,14 @@
 #This will have the functions for timing, and show popups
+import time
+
+start = time.time()
+minutes = 0
+tensecs = time.time()
+while True:
+    if time.time() - start > 60:
+        minutes += 1
+        print(f"{minutes} minutes have passed")
+        start = time.time()
+    if time.time() - tensecs > 10:
+        print('Ten seconds have passed')
+        tensecs = time.time()
