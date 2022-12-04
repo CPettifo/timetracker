6a99579a8e5556e4054808d2f22f89223e2a7f3e
Craig Pettifor <craig_petti4@hotmail.com>
2022-12-04 17:53:29 +0100

    moved logic from app.py to main.py

--- main.py
+++ main.py
@@ -5,7 +5,32 @@ import app
 
 def main():
     while True:
-        app.main_app()
+        main_app()
+
+def main_app():
+    #move this function to a different .py file. Have the .app be only for rendering app windows.
+    file = ''
+    while True:
+        # home_window returns 'change' to change the save directory
+        # 'modify' to change settings
+        # 'tracking' to start tracking
+        # 'log' to view the log
+        output = app.home_window()
+        if output == 'change':
+            file = app.file_path_window()
+            break
+        elif output == 'modify':
+            activity1, activity2, activity3 = app.settings_window("One", "Two", "Three")
+            break
+        elif output == 'log':
+            app.log_window()
+            break
+        elif output == 'export':
+            app.export_window()
+            break
+        elif output == 'tracking':
+            app.tracking_window()
+            break
 
 
 if __name__ == '__main__':
