a4c493b770266d2ec51275f14d9a65dbdcb8f25d
Craig Pettifor <craig_petti4@hotmail.com>
2022-12-05 14:10:32 +0100

    Successfully added class

--- main.py
+++ main.py
@@ -4,19 +4,35 @@ import sys
 
 #This file will manage the main logic, track the Activity classes and call functions from the timing and app files
 class Activity:
-    def __init__(self, name, hotkey, time):
+    def __init__(self, name, hotkey, hours, minutes):
         self.name = name
         self.hotkey = hotkey
-        self.time = time
+        self.hours = hours
+        self.minutes = minutes
 
     def __str__(self):
-        return f'"{self.name}" has "{self.hotkey}" as the hotkey, and you have recorded {self.time} today'
+        return f'"{self.name}" has "{self.hotkey}" as the hotkey, and you have recorded {self.minutes} minutes today'
+
+    #allows you the main function to add minutes to the activity
+    def __add__(self, n):
+        self.time += n
+    #@classmethod
+    #def get(cls):
+        #will call on function to make a new activity
 
 def main():
     #check the user-settings.json file exists
 
     #if it does not exist, render an app window to take initial settings
 
+
+    #testing initialisation of classes
+    coding = Activity("Coding", "ctrl + shift + F1", 3, 100)
+    writing = Activity("Writing", "ctrl + shift + F2", 1, 20)
+    french = Activity("French", "ctrl + shift + F3", 1, 20)
+    print(coding)
+    print(writing)
+    print(french)
     while True:
         main_app()
 
